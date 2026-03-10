189dfc5cc3921e1eeb13bb03c9de490a4fad1790
Sam Davis <davissam830@gmail.com>
2026-03-09 23:14:00 -0400

    add teams database

---
+++ app/data/app/data/teams.ts
@@ -0,0 +1,30 @@
+export const teams = [
+  {
+    name: "Duke",
+    adjO: 118.5,
+    adjD: 94.3,
+    tempo: 68.2,
+    logo: "https://a.espncdn.com/i/teamlogos/ncaa/500/150.png"
+  },
+  {
+    name: "UNC",
+    adjO: 116.2,
+    adjD: 96.5,
+    tempo: 70.4,
+    logo: "https://a.espncdn.com/i/teamlogos/ncaa/500/153.png"
+  },
+  {
+    name: "Kansas",
+    adjO: 120.1,
+    adjD: 95.4,
+    tempo: 69.5,
+    logo: "https://a.espncdn.com/i/teamlogos/ncaa/500/2305.png"
+  },
+  {
+    name: "UConn",
+    adjO: 119.6,
+    adjD: 92.7,
+    tempo: 67.8,
+    logo: "https://a.espncdn.com/i/teamlogos/ncaa/500/41.png"
+  }
+]
