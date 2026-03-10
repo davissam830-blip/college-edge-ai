6409107a56b5a24ad3fa3056afdf985bdfe4b935
Sam Davis <davissam830@gmail.com>
2026-03-10 18:35:44 -0400

    add teams file

--- app/app/data/teams.ts
+++
@@ -1,7 +0,0 @@
-export const teams = [
-  { name: "Duke", adjO: 118.5, adjD: 94.3, tempo: 68.2 },
-  { name: "Kansas", adjO: 120.1, adjD: 96.4, tempo: 69.5 },
-  { name: "Gonzaga", adjO: 121.4, adjD: 95.1, tempo: 71.2 },
-  { name: "UConn", adjO: 119.6, adjD: 92.7, tempo: 67.8 },
-  { name: "Houston", adjO: 115.8, adjD: 88.9, tempo: 63.5 }
-]
---
+++ app/data/teams.ts
@@ -0,0 +1,26 @@
+export const teams = [
+  {
+    name: "Duke",
+    adjO: 118.5,
+    adjD: 94.3,
+    tempo: 68.2
+  },
+  {
+    name: "UNC",
+    adjO: 116.2,
+    adjD: 96.5,
+    tempo: 70.4
+  },
+  {
+    name: "Kansas",
+    adjO: 120.1,
+    adjD: 95.4,
+    tempo: 69.5
+  },
+  {
+    name: "UConn",
+    adjO: 119.6,
+    adjD: 92.7,
+    tempo: 67.8
+  }
+]
